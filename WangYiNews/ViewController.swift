e9cc5d34590be61b8ac568c3e3ddee700caf9daa
zhl1179169386 <2259434901@qq.com>
2016-08-18 14:35:25 +0800

    Initial Commit

---
+++ WangYiNews/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WangYiNews
+//
+//  Created by yb on 16/8/18.
+//  Copyright © 2016年 朱德强. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
